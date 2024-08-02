101ed72d17297f4f532600186b617b5379826dec
TTiip <2711430287@qq.com>
2024-08-02 18:09:11 +0800

    feat: 支持根据环境配置 不同的环境变量参数&查询package.json包版本且自动升级命令

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CatsModule } from './cats/cats.module';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot({
+      // !!! env 文件必须放置在项目根目录下，切记(一个下午的调试时间换来的提醒!!!)
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+      isGlobal: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -6,17 +6,28 @@ import {
   Put,
   Param,
   Delete,
+  HttpCode,
+  Headers,
 } from '@nestjs/common';
 import { CreateCatDto, UpdateCatDto } from './dto';
 import type { Cat } from './interfaces';
 import { CatsService } from './cats.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('cats')
 export class CatsController {
-  constructor(private catsService: CatsService) {}
+  constructor(
+    private catsService: CatsService,
+    private configService: ConfigService,
+  ) {}
 
   @Post()
-  create(@Body() createCatDto: CreateCatDto) {
+  @HttpCode(200)
+  create(@Body() createCatDto: CreateCatDto, @Headers() headers) {
+    console.log(headers, 'headers');
+    console.log(createCatDto, 'createCatDto');
+    console.log(this.configService.get('MONGODB_URL'), 'MONGODB_URL');
+    console.log(process.env.NODE_ENV, 'process.env.NODE_ENV');
     this.catsService.create(createCatDto);
   }
 
